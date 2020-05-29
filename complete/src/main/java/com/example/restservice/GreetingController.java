c474c996ba1d554f98ac46f2b105a435e58557ab
anker <anker@dr-2.local>
2020-05-29 18:06:47 +0800

    test

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -14,6 +14,14 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		System.out.print("---");
+		System.out.print(name);
+		System.out.print("=====");
+//		if (name == "dreamer")
+		if (name.equals("dreamer"))
+		{
+			return new Greeting(counter.incrementAndGet(), String.format(template, "you're dreamer"));
+		}
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
